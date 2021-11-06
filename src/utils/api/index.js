fae56fc62fb8ca47fa3d784b9f519bab9802e78c
Derrick Holleman <derrickholleman@gmail.com>
2021-11-05 23:12:34 -0500

    added post request to utils

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,6 +1,6 @@
 // const URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
-const URL = "http://localhost:5000"
+const URL = "http://localhost:5000";
 
 export async function listUsers() {
   try {
@@ -26,5 +26,22 @@ export async function getAvgAge() {
     return await ageRes.json();
   } catch (err) {
     console.error(err);
+  }
+}
+
+export async function createUser(user) {
+  // const { name, email, etc } = user
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  try {
+    const newUser = await fetch(`${URL}/users`, options);
+    return await newUser.json();
+  } catch (err) {
+    console.error(error)
   }
 }
